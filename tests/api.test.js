811e6243bf385db9c447962c6d9f8188d202980c
marcato21 <levinkusuma@gmail.com>
2022-09-16 07:44:31 +0700

    increase code coverage

--- tests/api.test.js
+++ tests/api.test.js
@@ -8,6 +8,17 @@ const db = new sqlite3.Database(':memory:');
 const app = require('../src/app')(db);
 const buildSchemas = require('../src/schemas');
 
+let newRide = {
+    start_lat: 0, 
+    start_long: 0, 
+    end_lat: 0, 
+    end_long: 0,
+    rider_name: "Ferrari", 
+    driver_name: "John Doe", 
+    driver_vehicle: "Ferrari", 
+    created: new Date().toLocaleString('id-ID', {hour12: false})
+};
+
 describe('API tests', () => {
     before((done) => {
         db.serialize((err) => { 
@@ -27,6 +38,183 @@ describe('API tests', () => {
                 .get('/health')
                 .expect('Content-Type', /text/)
                 .expect(200, done);
+        });
+    });
+
+    describe('POST /rides', () => {
+        it('should create new data', (done) => {
+            newRide = {
+                start_lat: 0, 
+                start_long: 0, 
+                end_lat: 0, 
+                end_long: 0,
+                rider_name: "Ferrari", 
+                driver_name: "John Doe", 
+                driver_vehicle: "Ferrari", 
+                created: new Date().toLocaleString('id-ID', {hour12: false})
+            };
+            request(app)
+                .post('/rides')
+                .send(newRide)
+                .expect('Content-Type', /json/)
+                .expect(200)
+                .end((err, res) => {
+                    if (err) {
+                        return done(err)
+                    }
+                    done();
+                });
+        });
+
+        it('should get a input validation error for start latitude or longitude', (done) => {
+            newRide = {
+                start_lat: -100, 
+                start_long: 0, 
+                end_lat: 0, 
+                end_long: 0,
+                rider_name: "Ferrari", 
+                driver_name: "John Doe", 
+                driver_vehicle: "Ferrari", 
+                created: new Date().toLocaleString('id-ID', {hour12: false})
+            };
+            request(app)
+                .post('/rides')
+                .send(newRide)
+                .expect('Content-Type', /json/)
+                .expect(200)
+                .end((err, res) => {
+                    if (err) {
+                        return done(err)
+                    }
+                    done();
+                });
+        });
+
+        it('should get a input validation error for end latitude or longitude', (done) => {
+            newRide = {
+                start_lat: 0, 
+                start_long: 0, 
+                end_lat: -100, 
+                end_long: 0,
+                rider_name: "Ferrari", 
+                driver_name: "John Doe", 
+                driver_vehicle: "Ferrari", 
+                created: new Date().toLocaleString('id-ID', {hour12: false})
+            };
+            request(app)
+                .post('/rides')
+                .send(newRide)
+                .expect('Content-Type', /json/)
+                .expect(200)
+                .end((err, res) => {
+                    if (err) {
+                        return done(err)
+                    }
+                    done();
+                });
+        });
+
+        it('should get a input validation error for empty string for rider name', (done) => {
+            newRide = {
+                start_lat: 0, 
+                start_long: 0, 
+                end_lat: 0, 
+                end_long: 0,
+                rider_name: "", 
+                driver_name: "John Doe", 
+                driver_vehicle: "Ferrari", 
+                created: new Date().toLocaleString('id-ID', {hour12: false})
+            };
+            request(app)
+                .post('/rides')
+                .send(newRide)
+                .expect('Content-Type', /json/)
+                .expect(200)
+                .end((err, res) => {
+                    if (err) {
+                        return done(err)
+                    }
+                    done();
+                });
+        });
+
+        it('should get a input validation error for empty string for driver name', (done) => {
+            newRide = {
+                start_lat: 0, 
+                start_long: 0, 
+                end_lat: 0, 
+                end_long: 0,
+                rider_name: "Ferrari", 
+                driver_name: "", 
+                driver_vehicle: "Ferrari", 
+                created: new Date().toLocaleString('id-ID', {hour12: false})
+            };
+            request(app)
+                .post('/rides')
+                .send(newRide)
+                .expect('Content-Type', /json/)
+                .expect(200)
+                .end((err, res) => {
+                    if (err) {
+                        return done(err)
+                    }
+                    done();
+                });
+        });
+
+        it('should get a input validation error for empty string for driver vehicle', (done) => {
+            newRide = {
+                start_lat: 0, 
+                start_long: 0, 
+                end_lat: 0, 
+                end_long: 0,
+                rider_name: "Ferrari", 
+                driver_name: "John Doe", 
+                driver_vehicle: "", 
+                created: new Date().toLocaleString('id-ID', {hour12: false})
+            };
+            request(app)
+                .post('/rides')
+                .send(newRide)
+                .expect('Content-Type', /json/)
+                .expect(200)
+                .end((err, res) => {
+                    if (err) {
+                        return done(err)
+                    }
+                    done();
+                });
+        });
+    });
+
+    describe('GET /rides', () => {
+        it('should return list of rides data', (done) => {
+            request(app)
+                .get('/rides')
+                .expect('Content-Type', /json/)
+                .expect(200)
+                .end((err, res) => {
+                    if (err) {
+                        return done(err)
+                    }
+                    done();
+                });
+        });
+        
+    });
+
+    describe('GET /rides/:id', () => {
+        it('should GET rides with a given id', (done) => {
+            request(app)
+                .get(`/rides/${newRide.id}`)
+                .expect('Content-Type', /json/)
+                .expect(200)
+                .end((err, res) => {
+                    if (err) {
+                        return done(err)
+                    }
+                    done();
+                });
         });
     });
 });
